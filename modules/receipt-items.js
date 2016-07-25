12f591d4319c9cab67c31add2795dfeaef264b8a
Ruyu You <727809166@qq.com>
2016-07-26 07:35:34 +0800

    modify receiptItem.cartItems.* to receiptItem.get*()

--- modules/receipt-items.js
+++ modules/receipt-items.js
@@ -5,6 +5,26 @@ class ReceiptItem {
         this.saved = saved;
         this.subtotal = subtotal;
     }
+    
+    getBarcode () {
+        return this.cartItem.getBarcode();
+    }
+    
+    getName (){
+        return this.cartItem.getName();
+    }
+    
+    getUnit() {
+        return this.cartItem.getUnit();
+    }
+    
+    getPrice() {
+        return this.cartItem.getPrice();
+    }
+    
+    getCount() {
+        return this.cartItem.count;
+    }
 
     static buildReceiptItems(cartItems, allPromotions) {
         return cartItems.map(cartItem => {
